ff1e001fb97516799b4edb6ca84682b7ee995e24
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-08 13:21:26 +0700

    feat: template view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,89 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Tailwind Starter Template - Nordic Shop: Tailwind Toolbox</title>
+        <meta name="description" content="Free open source Tailwind CSS Store template">
+        <meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,starter template,free template,store template, shop layout, minimal, monochrome, minimalistic, theme, nordic">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <style>
+            .work-sans {
+                font-family: 'Work Sans', sans-serif;
+            }
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+            #menu-toggle:checked + #menu {
+                display: block;
+            }
+
+            .hover\:grow {
+                transition: all 0.3s;
+                transform: scale(1);
+            }
+
+            .hover\:grow:hover {
+                transform: scale(1.02);
+            }
+
+            .carousel-open:checked + .carousel-item {
+                position: static;
+                opacity: 100;
+            }
+
+            .carousel-item {
+                -webkit-transition: opacity 0.6s ease-out;
+                transition: opacity 0.6s ease-out;
+            }
+
+            #carousel-1:checked ~ .control-1,
+            #carousel-2:checked ~ .control-2,
+            #carousel-3:checked ~ .control-3 {
+                display: block;
+            }
+
+            .carousel-indicators {
+                list-style: none;
+                margin: 0;
+                padding: 0;
+                position: absolute;
+                bottom: 2%;
+                left: 0;
+                right: 0;
+                text-align: center;
+                z-index: 10;
+            }
+
+            #carousel-1:checked ~ .control-1 ~ .carousel-indicators li:nth-child(1) .carousel-bullet,
+            #carousel-2:checked ~ .control-2 ~ .carousel-indicators li:nth-child(2) .carousel-bullet,
+            #carousel-3:checked ~ .control-3 ~ .carousel-indicators li:nth-child(3) .carousel-bullet {
+                color: #000;
+                /*Set to match the Tailwind colour you want the active one to be */
+            }
+        </style>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            {{-- <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
-            </header>
+            </header> --}}
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+
+            @include('layouts.footer')
         </div>
     </body>
 </html>
