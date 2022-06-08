3c222d4c9d1315ff463236c990e3e41ac3595d02
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-08 14:46:02 +0700

    feat: add roles to user

---
+++ app/Http/Controllers/Auth/RedirectAuthenticatedUsersController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RedirectAuthenticatedUsersController extends Controller
+{
+    public function home()
+    {
+        if (auth()->user()->role == 'admin') {
+            return redirect('/dashboard/admin');
+        }
+        else if(auth()->user()->role == 'tenant') {
+            return redirect('/dashboard/tenant');
+        }
+        else if(auth()->user()->role == 'owner') {
+            return redirect('/dashboard/owner');
+        }
+        else {
+            return auth()->logout();
+        }
+    }
+}
