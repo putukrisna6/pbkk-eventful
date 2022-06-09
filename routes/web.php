f5ca41874cffe41ff6fbb673c8d5aba93bb31c5e
vincentyonathan <vincentyonathan24@gmail.com>
2022-06-09 18:00:35 +0700

    front end : detail

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\OwnerController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('detailgedung');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
