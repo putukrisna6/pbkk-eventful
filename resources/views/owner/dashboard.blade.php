dcf1fa49dbddf27513b1ffe5124cc7281e3e6e41
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-10 00:04:34 +0700

    feat: use flash message + delete buildings

--- resources/views/owner/dashboard.blade.php
+++ resources/views/owner/dashboard.blade.php
@@ -88,16 +88,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="notification blue">
-            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
-                <div>
-                    <span class="icon"><i class="mdi mdi-buffer"></i></span>
-                    <b>Responsive table</b>
-                </div>
-                <button type="button" class="button small textual --jb-notification-dismiss">Dismiss</button>
-            </div>
-        </div>
     </section>
 @endsection
 
