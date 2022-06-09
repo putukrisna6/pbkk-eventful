e1ac1362eed99cd0291df7e6d8d4b70b2acac5fe
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-09 12:47:41 +0700

    refactor: view structure

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -2,13 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Eventful | Admin Dashboard</title>
-
-    <link rel="stylesheet" href="{{ asset('asset/css/main.css?v=1628755089081') }}">
-    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css">
+    @include('shared.layouts.head')
+    <title>Eventful | Administrator Dashboard</title>
+    @yield('styles')
 </head>
 
 <body>
@@ -24,10 +20,8 @@
         @include('shared.layouts.footer')
     </div>
 
-    <script type="text/javascript" src="{{ asset('asset/js/main.min.js?v=1628755089081') }}"></script>
-
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-    <script type="text/javascript" src="{{ asset('asset/js/chart.sample.min.js') }}"></script>
+    @include('shared.layouts.scripts')
+    @yield('scripts')
 </body>
 
 </html>
