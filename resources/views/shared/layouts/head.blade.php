eeb3af56ab8e11e2f7cb64fcaa6ad7b7b4bbed40
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-09 13:20:28 +0700

    feat: add icon to dashboard

--- resources/views/shared/layouts/head.blade.php
+++ resources/views/shared/layouts/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
 <link rel="stylesheet" href="{{ asset('asset/css/main.css?v=1628755089081') }}">
 <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css">
