25b1558549870831a8a2ed26eb15c327ec095cfd
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-09 23:02:18 +0700

    feat: add building

--- app/Http/Controllers/BuildingsController.php
+++ app/Http/Controllers/BuildingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Building;
 
 class BuildingsController extends Controller
@@ -16,6 +17,34 @@ class BuildingsController extends Controller
         return view('owner.buildings.create');
     }
 
+    public function store(Request $request) {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'text' => ['required', 'string', 'max:1000'],
+            'address' => ['required', 'string', 'max:1000'],
+            'phone' => ['required', 'string', 'max:1000'],
+            'latitude' => ['required'],
+            'longitude' => ['required'],
+            'image' => ['required', 'image'],
+        ]);
+
+        $data = [
+            'name' => $request->name,
+            'text' => $request->text,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'image' => base64_encode(file_get_contents($request->file('image')->path())),
+            'user_id' => Auth::user()->id,
+        ];
+
+        $building = new Building;
+        $building->insert($data);
+
+        return redirect()->route('buildings.index');
+    }
+
     public function edit($id) {
         $building = Building::find($id);
         return view('owner.buildings.edit', compact('building'));
@@ -26,7 +55,7 @@ class BuildingsController extends Controller
         $building->update($request->all());
         return redirect()->route('buildings.index');
     }
-    
+
     public function show($id) {
         $building = Building::find($id);
         return view('owner.buildings.show', compact('building'));
--- resources/views/owner/buildings/create.blade.php
+++ resources/views/owner/buildings/create.blade.php
@@ -1,116 +1,147 @@
 @extends('owner.layouts.app')
 
 @section('content')
-    <section class="is-title-bar">
-        <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
-                <ul>
-                    <li>Buildings</li>
-                    <li>Add</li>
-                </ul>
-        </div>
-    </section>
-
     <section class="is-hero-bar">
         <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
             <h1 class="title">
-                Form Add Buildings
+                Add Building
             </h1>
         </div>
     </section>
 
     <section class="section main-section">
         <div class="card mb-6">
-        <header class="card-header">
-            <p class="card-header-title">
-            <span class="icon"><i class="mdi mdi-ballot"></i></span>
-            Forms
-            </p>
-        </header>
-        <div class="card-content">
-            <form method="get">
-            <div class="field">
-                <!-- <label class="label">From</label> -->
-                <div class="field-body">
-                    <div class="field">
-                        <div class="control icons-left">
-                            <input class="input" type="text" placeholder="Nama Gedung">
-                            <span class="icon left"><i class="mdi mdi-account"></i></span>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control">
-                            <textarea class="textarea" placeholder="Deskripsi Gedung"></textarea>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control icons-left">
-                            <input class="input" type="text" placeholder="Alamat Gedung">
-                            <span class="icon left"><i class="mdi mdi-account"></i></span>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control icons-left icons-right">
-                        <input class="input" type="email" placeholder="Email" value="alex@smith.com">
-                        <span class="icon left"><i class="mdi mdi-mail"></i></span>
-                        <!-- <span class="icon right"><i class="mdi mdi-check"></i></span> -->
-                        </div>
-                    </div>
+            <header class="card-header">
+                <p class="card-header-title">
+                    <span class="icon"><i class="mdi mdi-ballot"></i></span>
+                    Forms
+                </p>
+            </header>
+            <div class="card-content">
+                <form method="POST" action="{{ route('buildings.store') }}" enctype="multipart/form-data">
+                    @csrf
                     <div class="field">
                         <div class="field-body">
-                        <div class="field">
-                            <div class="field addons">
-                            <div class="control">
-                                <input class="input" value="+62" size="3" readonly>
-                            </div>
-                            <div class="control expanded">
-                                <input class="input" type="tel" placeholder="Your phone number">
-                            </div>
-                            </div>
-                            <p class="help">Do not enter the first zero</p>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">File Gambar Gedung</label>
-                            <div class="field-body">
-                                <div class="field file">
-                                    <label class="upload control">
-                                        <a class="button blue">
-                                        Upload
-                                        </a>
-                                        <input type="file">
-                                    </label>
+                            <div class="field">
+                                <div class="control icons-left">
+                                    <input class="input" id="name" name="name" :value="old('email')" required
+                                        autofocus type="text" placeholder="Building Name">
+                                    <span class="icon left"><i class="mdi mdi-domain"></i></span>
                                 </div>
                             </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Lokasi Gedung</label>
-                            <div class="field-body">
-                                <!-- TEMPAT GOOGLE MAPS -->
+                            <div class="field">
+                                <div class="control icons-left">
+                                    <textarea class="textarea" style="padding-left: 2.5rem" id="text" name="text" :value="old('text')" required
+                                        placeholder="Building Description"></textarea>
+                                    <span class="icon left"><i class="mdi mdi-text"></i></span>
+                                </div>
                             </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-            <div class="field grouped">
-                <div class="control">
-                <button type="submit" class="button green">
-                    Submit
-                </button>
-                </div>
-                <div class="control">
-                <button type="reset" class="button red">
-                    Reset
-                </button>
-                </div>
-            </div>
-            </form>
-        </div>
-        </div>
-    </section>    
 
+                            <div class="field">
+                                <div class="control icons-left">
+                                    <input class="input" id="phone" name="phone" :value="old('phone')" required
+                                        type="text" placeholder="Phone Number">
+                                    <span class="icon left"><i class="mdi mdi-phone"></i></span>
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control icons-left">
+                                    <input class="input" type="text" id="address" name="address" :value="old('address')" required placeholder="Address">
+                                    <span class="icon left"><i class="mdi mdi-city"></i></span>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Building Coordinates</label>
+                            </div>
+                            <div class="field grouped">
+                                <div class="control icons-left">
+                                    <input class="input" name="latitude" id="latitude" placeholder="Latitude"
+                                        required type="number" step="0.00000001" value="-6.232393">
+                                    <span class="icon left"><i class="mdi mdi-map-marker"></i></span>
+                                </div>
+                                <div class="control icons-left">
+                                    <input class="input" name="longitude" id="longitude" placeholder="Longitude"
+                                        required type="number" step="0.00000001" value="106.820287">
+                                    <span class="icon left"><i class="mdi mdi-map-marker"></i></span>
+                                </div>
+                                <div class="control">
+                                    <button type="button" onclick="initMap()" class="button light">
+                                        Locate
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="field-body">
+                                    <div id="map" style="height: 500px"></div>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">File Gambar Gedung</label>
+                                <div class="field-body">
+                                    <div class="field file">
+                                        <label class="upload control">
+                                            <a class="button blue">
+                                                Upload
+                                            </a>
+                                            <input id="image" name="image" type="file">
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="field grouped">
+                        <div class="control">
+                            <button type="submit" class="button green">
+                                Submit
+                            </button>
+                        </div>
+                        <div class="control">
+                            <button type="reset" class="button red">
+                                Reset
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
 @endsection
 
 @section('scripts')
-<script type="text/javascript" src="{{ asset('asset/js/chart.sample.min.js') }}"></script>
+    <script>
+        function handleEvent(event) {
+            document.getElementById('latitude').value = event.latLng.lat();
+            document.getElementById('longitude').value = event.latLng.lng();
+        }
+
+        function initMap() {
+            var latnya = document.getElementById('latitude').value;
+            var longinya = document.getElementById('longitude').value;
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 18,
+                center: {
+                    lat: parseFloat(latnya),
+                    lng: parseFloat(longinya)
+                }
+            });
+            var geocoder = new google.maps.Geocoder();
+
+            var marker = new google.maps.Marker({
+                map: map,
+                draggable: true,
+                position: {
+                    lat: parseFloat(latnya),
+                    lng: parseFloat(longinya)
+                }
+            });
+
+            marker.addListener('drag', handleEvent);
+        }
+    </script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCTnAg_gwZ-GQxB6xC0h2cY4TDFYU28ov8&callback=initMap"></script>
 @endsection
