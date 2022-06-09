57cff01a4b0c55f4ce03da5ad4620d03d88fddd7
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-10 00:29:10 +0700

    fix: section tag still rendered when flash empty

--- resources/views/shared/layouts/flash.blade.php
+++ resources/views/shared/layouts/flash.blade.php
@@ -1,29 +1,31 @@
-<section class="section main-section">
-    @if (session('error'))
-    <div class="notification red">
-        <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
-            <div>
-                <span class="icon"><i class="mdi mdi-buffer"></i></span>
-                <b>{{ session('error') }}</b>
+@if (session('error'))
+    <section class="section main-section">
+        <div class="notification red">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
+                <div>
+                    <span class="icon"><i class="mdi mdi-buffer"></i></span>
+                    <b>{{ session('error') }}</b>
+                </div>
+                <button type="button" class="button small textual --jb-notification-dismiss">
+                    <span class="icon left"><i class="mdi mdi-close"></i>
+                    </span>
+                </button>
             </div>
-            <button type="button" class="button small textual --jb-notification-dismiss">
-                <span class="icon left"><i class="mdi mdi-close"></i>
-                </span>
-            </button>
         </div>
-    </div>
-    @elseif (session('success'))
-    <div class="notification green">
-        <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
-            <div>
-                <span class="icon"><i class="mdi mdi-buffer"></i></span>
-                <b>{{ session('success') }}</b>
+    </section>
+@elseif (session('success'))
+    <section class="section main-section">
+        <div class="notification green">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
+                <div>
+                    <span class="icon"><i class="mdi mdi-buffer"></i></span>
+                    <b>{{ session('success') }}</b>
+                </div>
+                <button type="button" class="button small textual --jb-notification-dismiss">
+                    <span class="icon left"><i class="mdi mdi-close"></i>
+                    </span>
+                </button>
             </div>
-            <button type="button" class="button small textual --jb-notification-dismiss">
-                <span class="icon left"><i class="mdi mdi-close"></i>
-                </span>
-            </button>
         </div>
-    </div>
-    @endif
-</section>
+    </section>
+@endif
