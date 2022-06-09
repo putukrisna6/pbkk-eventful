5f7a610549dc13dcbc6fa611dc39284222a93c67
Thomas Dwi <mastomcsc23@gmail.com>
2022-06-09 14:06:34 +0700

    feat: rename class file to PascalCase

--- app/Models/buildings.php
+++ app/Models/buildings.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class buildings extends Model
+class Buildings extends Model
 {
     use HasFactory;
     
--- app/Models/options.php
+++ app/Models/options.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class options extends Model
+class Options extends Model
 {
     use HasFactory;
 
