27e4d439b038ebbfd2e864c5beb50da5f8358348
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-06-10 16:42:38 +0700

    feat: show available buildings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Building;
 
 class HomeController extends Controller
 {
-    //
+    public function welcome() {
+        $buildings = Building::where('status', array_search('AVAILABLE', Building::$STATUS))->get();
+        return view('welcome', compact('buildings'));
+    }
 }
