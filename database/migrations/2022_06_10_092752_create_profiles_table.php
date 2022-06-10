766909ec5b1fd79770098543dd786021872a7ce0
Thomas Dwi <mastomcsc23@gmail.com>
2022-06-10 17:03:51 +0700

    feat: prep prpfile tenant

---
+++ database/migrations/2022_06_10_092752_create_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->date('birthday');
+            $table->string('gender');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('profiles');
+    }
+};
