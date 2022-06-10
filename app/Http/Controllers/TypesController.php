dbd026a19405db93415607010b697ae350190a4c
Thomas Dwi <mastomcsc23@gmail.com>
2022-06-10 14:35:37 +0700

    feat: make edit form for types

--- app/Http/Controllers/TypesController.php
+++ app/Http/Controllers/TypesController.php
@@ -42,6 +42,41 @@ class TypesController extends Controller
         return redirect()->route('types.index');
     }
 
+    // function to edit
+    public function edit($id) {
+        $type = Type::find($id);
+        return view('admin.management.types.edit', compact('type'));
+    }
+
+    // function to update
+    public function update(Request $request, $id) {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255', 'unique:types,name,' . $id],
+                // 'image' => ['required', 'image'],
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            $message = '';
+            foreach ($e->errors() as $key => $error) {
+                $message = $message . $key . ': ' . $error[0];
+            }
+
+            session()->flash('error', $message);
+            return redirect()->route('types.index');
+        }
+
+        $type = Type::find($id);
+        $type->name = $request->name;
+        if ($request->hasFile('image')) {
+            $type->image = base64_encode(file_get_contents($request->file('image')->path()));
+        }
+
+        // $type->image = base64_encode(file_get_contents($request->file('image')->path()));
+        $type->save();
+        session()->flash('success', 'Type updated successfully.');
+        return redirect()->route('types.index');
+    }
+
     public function destroy($id) {
         $type = Type::find($id);
         $type->delete();
